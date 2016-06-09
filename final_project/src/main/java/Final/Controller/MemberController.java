019b17ccac095831e999eafe2b6031a4bf43e5c4
user2 <user2@KH2>
2016-06-09 18:06:39 +0900

    회원가입 부분 유효성검사 추가및 기능추가

--- final_project/src/main/java/Final/Controller/MemberController.java
+++ final_project/src/main/java/Final/Controller/MemberController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -51,7 +52,29 @@ public class MemberController
 		return "joinPage/join";
 	}
 	
-
+@RequestMapping("/confirmId.do")
+public String confirmID(Model model, HttpServletRequest request)
+{
+	System.out.println("실행중");
+	//아이디 중복 여부 변수
+	int check=0;
+	//view에서 받은 id변수 값 받아서 Stirng 형식의 id 변수에 저장
+	String id = request.getParameter("id");
+	MemberInfo member = null;
+	//DB에서 ID있는지 확인
+	member=memberDao.getMember(id);
+	
+	if(member==null){
+		//DB에 ID가 없는경우
+		check=1;
+	
+	}else{
+		//DB에 ID가 있는 경우
+		check=0;
+	}
+	model.addAttribute("check", check);
+	return "joinPage/confirmId";
+}
 	@RequestMapping("/modifyForm.do")
 	public ModelAndView modifyForm(HttpServletRequest request){
 		HttpSession session = request.getSession();
